f8a1d70e6183b9512c0db8ae390703c93492c410
Poorvik P <poorvikp508@gmail.com>
2026-05-23 15:13:27 +0530

    created new activity microservice

---
+++ activityservice/src/main/java/com/fitness/activityservice/model/ActicityType.java
@@ -0,0 +1,14 @@
+package com.fitness.activityservice.model;
+
+public enum ActicityType {
+    RUNNING,
+    WALKING,
+    CYCLING,
+    SWIMMING,
+    WEIGHT_TRAINING,
+    YOGA,
+    HIIT,
+    CARDIO,
+    STRETCHING,
+    OTHER
+}
